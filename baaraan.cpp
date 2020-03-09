15c7b2ac90db04a0fe3db5c0750430a8bf2d284d
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2020-03-09 23:06:47 +0100

    more renaming

---
+++ baaraan.cpp
@@ -0,0 +1,7 @@
+#include "baaraan.hpp"
+
+#include <iostream>
+
+void hello() {
+    std::cout << "Hello, World!" << std::endl;
+}
---
+++ include/baaraan.hpp
@@ -0,0 +1,10 @@
+#ifndef BAARAAN_BAARAAN_HPP
+#define BAARAAN_BAARAAN_HPP
+
+#include "dists/mvnorm_distribution.h"
+#include "dists/truncated_mvnorm_distribution.h"
+#include "dists/mv_t_distribution.h"
+
+void hello();
+
+#endif //BAARAAN_BAARAAN_HPP
