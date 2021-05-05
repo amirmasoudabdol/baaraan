fcf4a0caacc251f7d0acfa3ebb78ce904ed0d73c
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-05-05 11:27:06 +0200

    changing the structure of the project, making a proper library, at least trying to

---
+++ include/baaraan/random.h
@@ -0,0 +1,14 @@
+//
+// Created by Amir Masoud Abdol on 05/05/2021.
+//
+
+#ifndef BAARAAN_RANDOM_H
+#define BAARAAN_RANDOM_H
+
+#include "../dists/mv_t_distribution.h"
+#include "../dists/mvnorm_distribution.h"
+#include "../dists/rectified_normal_distribution.h"
+#include "../dists/truncated_mvnorm_distribution.h"
+#include "../dists/truncated_normal_distribution.h"
+
+#endif // BAARAAN_RANDOM_H
